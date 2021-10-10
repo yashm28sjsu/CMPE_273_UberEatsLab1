5c43d56d35e17dba42ff5716f3f2122fe99f9090
yashm28sjsu <yashkishorbhai.modi@sjsu.edu>
2021-10-10 16:50:52 -0700

    Cors fix

--- backend/app/app.js
+++ backend/app/app.js
@@ -10,14 +10,14 @@ const orderRoutes = require('./routes/order');
 
 const app = express();
 // use cors to allow cross origin resource sharing
-app.use(cors({ origin: 'http://localhost:3001', credentials: true }));
+// app.use(cors({ origin: 'http://localhost:3001', credentials: true }));
 app.use(cors({ origin: 'http://3.135.61.25:3001', credentials: true }));
 app.use(express.json());
 const port = 3000;
 
 // Allow Access Control
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+  res.setHeader('Access-Control-Allow-Origin', 'http://3.135.61.25:3001');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader(
     'Access-Control-Allow-Methods',
